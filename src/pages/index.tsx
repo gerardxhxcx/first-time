8d56bc9d13f68854fed95a021e59e3feacd90d31
gerardxhxcx@gmail.com <gerardxhxcx@gmail.com>
2022-12-26 11:12:57 -0500

    Gatsby tutorial 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,18 @@ import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 import HelloWorld from "../components/HelloWorld"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout pageTitle="Home Page">
       <HelloWorld />
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
+      <StaticImage alt="leaviathan" src="../images/leviathan.jpg" width={500} height={280}/>
     </Layout>
   )
 }
