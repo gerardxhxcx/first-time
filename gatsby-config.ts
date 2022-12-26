d4214398c7c987b68a2ae85fbb2df2a4624e5ef3
gerardxhxcx@gmail.com <gerardxhxcx@gmail.com>
2022-12-26 15:18:18 -0500

    Gatsby tutorial 7

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -23,6 +23,7 @@ const config: GatsbyConfig = {
       }
     },
     `gatsby-plugin-mdx`,
+    `gatsby-transformer-sharp`,
   ],
 }
 
--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,12 +1,24 @@
 import { graphql, PageProps, StaticQueryDocument } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import * as React from 'react'
 import Layout from '../../components/Layout'
 import Seo from '../../components/Seo'
 
 const BlogPost: React.FC<{data:any, children: string}> = ({data, children}) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage
+        image={image}
+        alt={data.mdx.frontmatter.hero_image_alt}
+      />
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       {children}
     </Layout>
   )
@@ -20,6 +32,14 @@ export const query: StaticQueryDocument = graphql`
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
