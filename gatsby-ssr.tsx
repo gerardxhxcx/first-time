33f4216581ecfa35136db108448d78c3ab7e59b6
gerardxhxcx@gmail.com <gerardxhxcx@gmail.com>
2022-12-26 15:42:50 -0500

    Gatsby adding land and body attrs 2

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -5,7 +5,7 @@ const HtmlAttributes = {
 }
 
 const HeadComponents = [
-    <script key="my-script" src="https://gatsby.dev/my-script" />
+    // <script key="my-script" src="https://gatsby.dev/my-script" />
 ]
 
 const BodyAttributes = {
